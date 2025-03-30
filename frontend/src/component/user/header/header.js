956f029e6baf77158375d31c430f76cb31c0d51e
22120209 <minhpqdp02@gmail.com>
2025-03-30 23:17:09 +0700

    them logo cho frontend

--- frontend/src/component/user/header/header.js
+++ frontend/src/component/user/header/header.js
@@ -1,13 +1,25 @@
-// src/Header.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
+  const [logoUrl, setLogoUrl] = useState(null);
+
+  useEffect(() => {
+    // Gọi API để lấy link ảnh
+    fetch(`${process.env.REACT_APP_API_URL}/api/logo_app`)
+      .then(response => response.json())
+      .then(data => {
+          setLogoUrl(data);
+      })
+      .catch(error => console.error('Error fetching logo:', error));
+  }, []);
+
   return (
     <header>
-     
-     <Link to='/' className='text-decoration-none text-center original text-dark'><h1>ORIGINAL</h1></Link>
+      <Link to='/' className='text-decoration-none text-center original text-dark'>
+        {logoUrl ? <img src={logoUrl} alt="Logo" className="logo" /> : <h1>ORIGINAL</h1>}
+      </Link>
     </header>
   );
 }
